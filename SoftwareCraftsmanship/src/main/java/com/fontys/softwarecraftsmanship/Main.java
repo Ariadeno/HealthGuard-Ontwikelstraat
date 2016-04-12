a7844a45ae3b4fca504c53989e229a2e4705112e
charlesgroothuijsen <charlesgroothuijsen@gmail.com>
2016-04-12 12:36:39 +0200

    Refactor

--- SoftwareCraftsmanship/src/main/java/com/fontys/softwarecraftsmanship/Main.java
+++ SoftwareCraftsmanship/src/main/java/com/fontys/softwarecraftsmanship/Main.java
@@ -8,12 +8,10 @@ public class Main {
         IntWrapper intWrapperObject = new IntWrapper();
         intWrapperObject.intValue = 2;
         Computer computerObject = new Computer();
-
         computerObject.AddPart(new Part("Casing", "Cooler Master Centurion 6", 49.50, "Midi tower"));
         computerObject.AddPart(new Part("Processor", "Intel i7-4790", 295.0, 3.6));
         computerObject.AddPart(new Part("Memory", "G.SKILL Ripjaws X Series", 99.99, "DDR3", 16));
         computerObject.AddPart(new Part("Motherboard", "MSI H97 PC MATE - Socket 1150 - ATX", 89.0));
-
         if (computerObject.IsComplete()) {
           intWrapperObject.intValue = 1;
         }
